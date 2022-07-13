1b1763a215527bd527404a9a5770f44e5854b1b8
Lieke Michielsen <42767830+lcmmichielsen@users.noreply.github.com>
2022-07-13 09:41:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
