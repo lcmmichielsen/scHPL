22afccbbb923beb48b070d0b0a7d1a27c8b1f3d1
lcmmichielsen <lieke.michielsen@hotmail.com>
2023-11-29 09:21:54 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,13 +18,13 @@ from datetime import datetime
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Readthedocs theme -------------------------------------------------------
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+import sphinx_rtd_theme
 
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 import scHPL
 
